4f1561e7c6eb8d7bfa92c5e2bd02707fda0f5c45
jeevanearform <99179126+jeevanearform@users.noreply.github.com>
2023-09-28 11:33:52 +0100

    app displays secret replicated

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const port = process.env.PORT || 8080;
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello, World!\nDB UserName: ' + process.env.db_uname + '\nDB Password: ' + process.env.db_pwd  );
+  res.end('Hello, World!\nDB UserName: ' + process.env.db_uname + '\nDB Password: ' + process.env.db_pwd + '\n' );
   
 });
 
